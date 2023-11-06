7424b59f44994436f30e224eab4959be11c55d29
Amazing-lab <142375800+Amazing-lab@users.noreply.github.com>
2023-11-06 17:40:09 +0000

    Edited ban cmd

--- commands/Moderation/Ban.js
+++ commands/Moderation/Ban.js
@@ -4,14 +4,31 @@ module.exports = [{
     code: `
 
 $ban[$guildID;$findMember[$message[1];false];7;$replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;No reason provided];false;$messageSlice[1]]]
-$sendMessage[<@$replaceText[$replaceText[$findMember[$message[1];false];<@;];>;]> has been **banned.** Reason: $replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;No reason provided];false;$messageSlice[1]]{newEmbed:{footer:Moderator#COLON# $username:$userAvatar[$authorID]}{timestamp}}]
 
+
+$title[Member Banded...!]
+$description[
+<@$replaceText[$replaceText[$findMember[$message[1];false];<@;];>;]> has been **banned by <@$authorID>**
+Reason: $replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;No reason provided];false;$messageSlice[1]
+]
+$addTimestamp
 
 $onlyPerms[banmembers;Uh oh...You do not have \`BAN_MEMBERS\` permission to use this command.{extraOptions:{delete:5s}}]
 $onlyIf[$hasPerms[$guildID;$clientID;banmembers]==true;I do not have \`BAN_MEMBERS\` permission.{extraOptions:{delete:5s}}]
+
+$onlyIf[$rolePosition[$userHighestRole[$clientID]]!=$rolePosition[$userHighestRole[$findMember[$message[1];true;$guildID]]];Uh oh... I can't ban someone same highest role as mine.{extraOptions:{delete:5s}}]
+
 $onlyIf[$rolePosition[$userHighestRole[$clientID]]<$rolePosition[$userHighestRole[$findMember[$message[1];true;$guildID]]];Uh oh... The user has higher role than me.{extraOptions:{delete:5s}}]
+
 $onlyIf[$rolePosition[$userHighestRole[$authorID]]<$rolePosition[$userHighestRole[$findMember[$message[1];true;$guildID]]];Uh oh... The user has higher role than you.{extraOptions:{delete:5s}}]
 $onlyIf[$findUser[$message[1];true]!=$authorID;Do not try to do awkward things like banning yourself.{extraOptions:{delete:5s}}]
+
+
+
 $onlyIf[$memberExists[$findUser[$message[1];false]]==true;The user is not in the server.{extraOptions:{delete:5s}}]
-$onlyIf[$message[1]!=;The syntax is \`ban [mention/user ID] (reason)\`]`
+$onlyIf[$message[1]!=;The syntax is \`ban [mention/user ID] (reason)\`]
+
+$onlyIf[$isBanned[$guildID;$findUser[$message[1];yes]]!=true;This user is already banned]
+
+`
 }]
