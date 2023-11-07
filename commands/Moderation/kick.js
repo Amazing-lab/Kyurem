313916f475b8cdd7f430d06671ce949607bb52a3
Amazing-lab <142375800+Amazing-lab@users.noreply.github.com>
2023-11-07 14:47:02 +0000

    Updated kick.js

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -13,15 +13,15 @@ $description[
 $color[$getVar[color]]
 $addtimestamp
 
-  $onlyPerms[kickmembers;{newEmbed{description:You do not have \`KICK_MEMBERS\` permission.}}{extraOptions:{delete:5s}}]
+  $onlyPerms[kickmembers;{newEmbed{description:<a:error:1171148997918982155>  You do not have \`KICK_MEMBERS\` permission.}}{extraOptions:{delete:5s}}]
 
-  $onlyIf[$hasPerms[$guildID;$clientID;kickmembers]==true;Uh..oh... I do not have \`KICK_MEMBERS\` permission.{extraOptions:{delete:5s}}]
+  $onlyIf[$hasPerms[$guildID;$clientID;kickmembers]==true;<a:error:1171148997918982155>  I do not have \`KICK_MEMBERS\` permission.{extraOptions:{delete:5s}}]
 
-  $onlyIf[$rolePosition[$userHighestRole[$clientID]]<$rolePosition[$userHighestRole[$findMember[$message[1];true;$guildID]]];Uh oh... The user has higher role than me.{extraOptions:{delete:5s}}]
+  $onlyIf[$rolePosition[$userHighestRole[$clientID]]<$rolePosition[$userHighestRole[$findMember[$message[1];true;$guildID]]];<a:error:1171148997918982155>  The user has higher role than me.{extraOptions:{delete:5s}}]
 
-  $onlyIf[$rolePosition[$userHighestRole[$authorID]]<$rolePosition[$userHighestRole[$findMember[$message[1];true;$guildID]]];You can't kick the person that has higher role position than you.{extraOptions:{delete:5s}}]
-  $onlyIf[$findUser[$message[1];true]!=$authorID;Do not try to do awkward things like kicking yourself.{extraOptions:{delete:5s}}]
+  $onlyIf[$rolePosition[$userHighestRole[$authorID]]<$rolePosition[$userHighestRole[$findMember[$message[1];true;$guildID]]];<a:error:1171148997918982155>  You can't kick the person that has higher role position than you.{extraOptions:{delete:5s}}]
+  $onlyIf[$findUser[$message[1];true]!=$authorID;<:Huh:1171150436355539047> Do not try to do awkward things like kicking yourself.{extraOptions:{delete:5s}}]
   $onlyIf[$memberExists[$findUser[$message[1];false]]==true;The user is not in the server.{extraOptions:{delete:5s}}]
-  $onlyIf[$message[1]!=;The syntax is \`kick [mention/user id] (reason)\`]
+  $onlyIf[$message[1]!=;<a:error:1171148997918982155>  The correct usage is \`kick [mention/user id] (reason)\`]
 `
 }]
